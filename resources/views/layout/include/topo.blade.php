8a83ff861abb30c244e73f712378e5914b93bc39
GuilhermeCesar029 <guicesar029@gmail.com>
2020-06-26 17:23:12 -0300

    Tela para recuperação de senha

--- resources/views/layout/include/topo.blade.php
+++ resources/views/layout/include/topo.blade.php
@@ -49,17 +49,19 @@
               <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                   {{ Auth::user()->name }} <span class="caret"></span>
               </a>
-
+        
               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                   <a class="dropdown-item" href="{{ route('logout') }}"
                      onclick="event.preventDefault();
                                    document.getElementById('logout-form').submit();">
-                      {{ __('Logout') }}
+                      {{ __('Sair') }}
                   </a>
 
                   <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                       @csrf
                   </form>
+
+                  <a class="dropdown-item" href="#">Carrinho</a>
               </div>
           </li>
           @endif

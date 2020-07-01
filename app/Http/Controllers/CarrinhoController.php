acd8bb14ce22188deb72780c18fe2b9bd7423bbb
GuilhermeCesar029 <guicesar029@gmail.com>
2020-07-01 14:48:16 -0300

    função para adicionar produto ao carrinho

--- app/Http/Controllers/CarrinhoController.php
+++ app/Http/Controllers/CarrinhoController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Auth;
 use App\Pedido;
+use App\Product;
+use App\PedidoProduto;
 
 class CarrinhoController extends Controller
 {
@@ -23,4 +25,53 @@ class CarrinhoController extends Controller
     
         return view('carrinho.index', compact('pedidos'));
     }
+
+    public function adicionar(Request $request){
+        //verifica de requisição enviada tem um token de segurança
+        $this->middleware('VerifyCsrfToken');
+
+        $idproduto = $request->input('id');
+
+        $produto = Product::find($idproduto);
+        // se o produto nao for encontrado ele ira retornar para a pagina carrinho.index
+        if( empty($produto->id) ){
+            $request->session()->flash('mensagem-falha', 'Produto não encontrado em nossa loja!'); //flash mensagem temporaria
+
+            return redirect()->route('carrinho.index');
+        }
+        
+        $idususario = Auth::id();
+
+        //se tiver pedido reservado deste ususario
+        $idpedido = Pedido::consultaId([
+            'user_id' => $idususario,
+            'status'  => 'RE'
+        ]);
+
+        //se nao tiver pedido reservado
+        if( empty($idpedido) ){
+            $pedido_novo = Pedido::create([
+                'user_id' => $idususario,
+                'status'  =>'RE'
+            ]);
+
+            $idpedido = $pedido_novo->id;
+        }
+
+        //criando relacionamento
+        PedidoProduto::create([
+            'pedido_id'  => $idpedido,
+            'produto_id' => $idproduto,
+            'valor'      => $produto->valor,
+            'status'     => 'RE'
+        ]);
+
+        //mensagem de sucesso
+
+        $request->session()->flash('mensagem-sucesso', 'Produto adicionado ao carrinho com sucesso!');
+
+        return redirect()->route('carrinho.index');
+
+    }
+
 }

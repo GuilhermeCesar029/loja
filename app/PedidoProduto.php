313cf445767b50ce36b08b87d081ddce98741c8a
GuilhermeCesar029 <guicesar029@gmail.com>
2020-07-01 14:17:16 -0300

    campos obrigatorios

--- app/PedidoProduto.php
+++ app/PedidoProduto.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class PedidoProduto extends Model
 {
+    protected $fillable = [
+        'pedido_id',
+        'produto_id',
+        'status',
+        'valor'
+    ];
     //relacionamento da foreign Key(Pedido_id) para a Primary Key(id)
     //pesquisando na tabela produtos o produto informado
     public function produto(){
